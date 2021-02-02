9332c999296fc6bd92ab3c4a634af62208f4dfa8
Katsuki Kobayashi <rare@tirasweel.org>
2021-02-03 00:40:24 +0900

    fix TreeNode, and implement free_vec_str() method

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::str::FromStr;
+
+pub type TreeNodeI32 = TreeNode<i32>;
 
 // Definition for a binary tree node.
 #[derive(Debug, PartialEq, Eq)]
@@ -20,7 +23,7 @@ impl<T: Copy> TreeNode<T> {
     }
 }
 
-impl<T: Copy + std::fmt::Debug> TreeNode<T> {
+impl<T: Copy + std::fmt::Debug + FromStr> TreeNode<T> {
     pub fn from_vec(tree: Vec<Option<T>>) -> Option<Rc<RefCell<Self>>> {
         if tree.is_empty() {
             return None;
@@ -36,11 +39,11 @@ impl<T: Copy + std::fmt::Debug> TreeNode<T> {
         }
 
         for i in 0..(tree.len() / 2) {
-            if let Some(p) = &nodes[i] {
-                if let Some(n) = &nodes[2 * (i + 1) - 1] {
+            if let Some(Some(p)) = nodes.get(i) {
+                if let Some(Some(n)) = nodes.get(2 * (i + 1) - 1) {
                     p.clone().borrow_mut().left = Some(n.clone());
                 }
-                if let Some(n) = &nodes[2 * (i + 1)] {
+                if let Some(Some(n)) = nodes.get(2 * (i + 1)) {
                     p.clone().borrow_mut().right = Some(n.clone());
                 }
             }
@@ -48,10 +51,31 @@ impl<T: Copy + std::fmt::Debug> TreeNode<T> {
 
         nodes[0].clone()
     }
+
+    pub fn from_vec_str(tree_str: &str) -> Option<Rc<RefCell<Self>>> {
+        let tree_string = tree_str.trim();
+
+        // string should be en_stringclosed by brackets
+        let tree_string = tree_string.strip_prefix("[")?;
+        let tree_string = tree_string.strip_suffix("]")?;
+
+        let elements = tree_string.split(',').collect::<Vec<&str>>();
+
+        let mut tree = Vec::new();
+
+        for e in elements {
+            tree.push(match e.trim() {
+                "null" => None,
+                _ => Some(e.trim().parse::<T>().ok()?),
+            });
+        }
+
+        TreeNode::from_vec(tree)
+    }
 }
 
 #[cfg(test)]
-mod tests {
+mod tests_treenode {
     use crate::TreeNode;
 
     use std::cell::RefCell;
@@ -113,6 +137,38 @@ mod tests {
                     left: Some(Rc::new(RefCell::new(TreeNode::new(6)))),
                     right: Some(Rc::new(RefCell::new(TreeNode::new(7)))),
                 }))),
+            })))
+        );
+    }
+
+    #[test]
+    fn integer_single_node_from_vec_str() {
+        assert_eq!(
+            TreeNode::from_vec_str("[1]"),
+            Some(Rc::new(RefCell::new(TreeNode::new(1))))
+        );
+    }
+
+    #[test]
+    fn integer_three_nodes_with_none_from_vec_str() {
+        assert_eq!(
+            TreeNode::from_vec_str("[1, null, 3]"),
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: None,
+                right: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+            })))
+        );
+    }
+
+    #[test]
+    fn integer_two_nodes_from_vec_str() {
+        assert_eq!(
+            TreeNode::from_vec_str("[1, 3]"),
+            Some(Rc::new(RefCell::new(TreeNode {
+                val: 1,
+                left: Some(Rc::new(RefCell::new(TreeNode::new(3)))),
+                right: None,
             })))
         );
     }
